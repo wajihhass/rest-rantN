cccffc97178fab7d5c9ee5e3ac41652833521e32
Wajih <hass_wajih@hotmail.com>
2022-03-10 11:23:03 -0500

    Add commit

--- models/index.js
+++ models/index.js
@@ -1,21 +1,18 @@
 
-//require('dotenv').require()
+require('dotenv').config()
+
+//DATABASE
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI
 mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
     console.log(`connected to MongoDB at: ${MONGO_URI}`)
 })
 
-
-
-
-
-
-
-
-//module.exports.Place = require('./places')
-
 const Place=require('./places')
+const Comment=require('./comment')
+
+
 module.exports = {
-    Place
+    Place,
+    Comment
 }
--- models/places.js
+++ models/places.js
@@ -8,16 +8,29 @@ const { Schema } = mongoose
 
 
 const placeSchema = new Schema({
-  name: { type: String, required: true },
-  pic: { type: String, default: 'http://placekitten.com/350/350' },
-  cuisines: { type: String, required: true },
-  city: { type: String, default: 'Anytown' },
-  state: { type: String, default: 'USA' },
+  name: { 
+    type: String, required: true
+   },
+  pic: {
+     type: String, default: 'http://placekitten.com/350/350'
+     },
+  cuisines: {
+     type: String, required: true
+     },
+  city: {
+     type: String, default: 'Anytown'
+     },
+  state: {
+     type: String, default: 'USA' 
+    },
   founded: {
     type: Number,
     min: [1673, 'Surely not that old?!'],
     max: [new Date().getFullYear(), 'Hey, this year is in the future!']
-  }
+  },
+  comments: [{ 
+    type: mongoose.Schema.Types.ObjectId, ref: 'Comment' 
+  }]
 })
 
 placeSchema.methods.showEstablished = function() {
